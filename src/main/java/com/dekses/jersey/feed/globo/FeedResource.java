700e5a15954668b1b01436cbcacd60c35eefbf9c
Ricardo Baltazar Chaves <ricardobchaves6@gmail.com>
2017-08-24 23:41:33 -0300

    Unificando código em uma classe e corrigindo erro no fomarto do json

--- src/main/java/com/dekses/jersey/feed/globo/FeedResource.java
+++ src/main/java/com/dekses/jersey/feed/globo/FeedResource.java
@@ -1,11 +1,7 @@
 package com.dekses.jersey.feed.globo;
 
 import java.io.IOException;
-import java.io.StringWriter;
 import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.Iterator;
-import java.util.function.Consumer;
 
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
@@ -13,27 +9,11 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.XML;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.w3c.dom.Document;
 import org.xml.sax.SAXException;
 
-import com.dekses.jersey.feed.globo.providers.JsonProvider;
-import com.dekses.jersey.feed.globo.providers.XMLProvider;
-
-import converters.Converter;
+import converters.XMLConvert;
 
 
 /**
@@ -56,25 +36,11 @@ public class FeedResource {
      * @throws MalformedURLException 
      */ 
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON + ";charset=utf-8")
     public String getIt() throws Exception {
     	
-    	XMLProvider xmlprovider = new XMLProvider();
-    
-    	String xml = xmlprovider.GetXML(url);
-   	
-    	//PEGA DENTRO DO JSONOBJECT O ARRAY DE ITENS
-    	JsonProvider jsonprovider = new JsonProvider();
-    	JSONArray itens = jsonprovider.GetArrayDeItens(xml);
+    	XMLConvert converte = new XMLConvert();
 
-    	Converter converter = new Converter();
-    	JSONArray feed = converter.ConverteItemsToFeedItem(itens);
-
-    	JSONObject root = new JSONObject();
-    	root.put("feed", feed);
-
-    	String t = root.toString() ;
-    	//System.out.println(t);
-        return t;
+        return converte.converteXMLtoJson(url);
     }
 }
---
+++ src/main/java/converters/XMLConvert.java
@@ -0,0 +1,131 @@
+package converters;
+
+import java.io.StringWriter;
+import java.net.URL;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.XML;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.w3c.dom.Document;
+
+public class XMLConvert {
+
+	public String converteXMLtoJson(String url) throws Exception {
+    	
+    	//Faz o download do XML e entre ele em String
+    	String xml = downloadAsString(url);
+
+    	//Pega a lista de itens do XML em formato JSON
+    	JSONArray itens = getArrayDeItens(xml);
+    	
+    	JSONArray feed = converteItemsToFeedItem(itens);
+
+    	JSONObject root = new JSONObject();
+    	root.put("feed", feed);
+    	
+    	return root.toString();
+	}
+	
+	private JSONArray getArrayDeItens(String xml) {
+		 //AGORA ELE CONVERTE O XML QUE ESTÁ EM UMA STRING PARA UM JSONOBJECT
+	   	JSONObject soapDatainJsonObject = XML.toJSONObject(xml);
+	
+	   	//PEGA DENTRO DO JSONOBJECT O ARRAY DE ITENS
+	   	JSONArray itens =  soapDatainJsonObject.getJSONObject("rss").getJSONObject("channel").getJSONArray("item");
+	   	
+	   	return itens;
+	}
+	
+	private String downloadAsString(String url) throws Exception {
+		
+		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        Document doc = db.parse(new URL(url).openStream());
+        
+        TransformerFactory tf = TransformerFactory.newInstance();
+        Transformer transformer = tf.newTransformer();
+        
+        StringWriter writer = new StringWriter();
+        transformer.transform(new DOMSource(doc), new StreamResult(writer));
+        
+        return writer.getBuffer().toString();
+	}
+	
+	private JSONArray converteItemsToFeedItem(JSONArray itens) {
+		
+		JSONArray feed = new JSONArray();
+    	
+    	//PARA CADA ITEM ELE VAI MONTAR O NOVO OBJETO
+    	for (int i = 0; i < itens.length(); i++) {
+	
+			JSONArray description = new JSONArray();
+			
+			//PEGANDO O CAMPO DESCRIPTION PARA REMOVER OS DADOS    			
+			org.jsoup.nodes.Document doc1 = Jsoup.parse(itens.getJSONObject(i).getString("description"));
+
+			//SELECIONANDO TODOS OS <p>
+			Elements paragrafos = doc1.select("p");
+        	for (Element paragrafo : paragrafos) {
+        		
+        		if(!paragrafo.text().equals("\u00a0")) {
+        			JSONObject pjson = new JSONObject();
+            		pjson.put("type", "text");
+            		pjson.put("content", paragrafo.text());
+            		description.put(pjson);	
+        		}
+        		
+        		
+            }
+        	
+        	//SELECIONANDO TODOS OS <div><img>
+        	Elements images = doc1.select("div").select("img");
+        	for (Element image : images) {
+        		JSONObject ijson = new JSONObject();
+        		ijson.put("type", "image");
+        		ijson.put("content", image.attr("src"));
+        		description.put(ijson);
+            }
+        	
+        	//SELECIONANDO TODOS OS <div><ul><li><a>
+			Elements links = doc1.select("div").select("ul").select("li").select("a");
+			JSONArray listalinks = new JSONArray();
+        	for (Element link : links) {
+
+        		listalinks.put(link.attr("abs:href"));
+            }
+    		JSONObject ljson = new JSONObject();
+    		ljson.put("type", "links");
+    		ljson.put("content", listalinks);
+    		
+        	description.put(ljson);
+        	
+        	
+        	org.jsoup.nodes.Document titulo = Jsoup.parse(itens.getJSONObject(i).getString("title"));
+        	org.jsoup.nodes.Document link = Jsoup.parse(itens.getJSONObject(i).getString("link"));
+        	
+        	
+			JSONObject item = new JSONObject();
+			item.put("title", titulo.text());
+			item.put("link", link.text());
+			item.put("description", description);
+        	
+			JSONObject itemroot = new JSONObject();
+			itemroot.put("item", item);
+			
+			feed.put(itemroot);
+    		
+    	}
+    	
+    	return feed;
+	}
+}
